bdc87b5bc2c9576a8f6b046fbfa51adfdf193a04
Adam Teodoro <adamteodoronunes@gmail.com>
2022-07-18 21:23:31 -0300

    fix slide effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 
 import {
@@ -26,35 +26,7 @@ export class AppComponent {
         lastAccess: dateString
       });
     })();
-    setTimeout(() => {
-      const main = this.getElement("main");
-      this.slideElement(main);
-    }, 500);
   }
 
-  getElement(name: string) {
-    return document.getElementById(name);
-  }
-
-  slideElement(element: HTMLElement) {
-    element.style.setProperty("margin-right", '0%');
-    element.style.setProperty("margin-left", '2%');
-  }
-
-  @HostListener('window:scroll', ['$event']) onScroll(event: any) {
-    try {
-      const projects = this.getElement("projects");
-      const learn = this.getElement("learn");
-      const scrollBarTopDistance = document.documentElement.scrollTop;
-      const projectsTopDistance = projects.offsetTop;
-      const learnTopDistance = learn.offsetTop;
-      if (scrollBarTopDistance >= projectsTopDistance - (projectsTopDistance / 3)) {
-        this.slideElement(projects);
-      }
-      if (scrollBarTopDistance >= learnTopDistance - (learnTopDistance / 3)) {
-        this.slideElement(learn);
-      }
-    } catch { }
-  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +9,37 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    window.scroll({ top: 0 });
 
+    setTimeout(() => {
+      const main = this.getElement("main");
+      this.slideElement(main);
+    }, 500);
+  }
 
+  getElement(name: string) {
+    return document.getElementById(name);
+  }
+
+  slideElement(element: HTMLElement) {
+    element.style.setProperty("margin-right", '0%');
+    element.style.setProperty("margin-left", '2%');
+  }
+
+  @HostListener('window:scroll', ['$event']) onScroll(event: any) {
+    try {
+      const projects = this.getElement("projects");
+      const learn = this.getElement("learn");
+      const scrollBarTopDistance = document.documentElement.scrollTop;
+      const projectsTopDistance = projects.offsetTop;
+      const learnTopDistance = learn.offsetTop;
+      if (scrollBarTopDistance >= projectsTopDistance - (projectsTopDistance / 3)) {
+        this.slideElement(projects);
+      }
+      if (scrollBarTopDistance >= learnTopDistance - (learnTopDistance / 3)) {
+        this.slideElement(learn);
+      }
+    } catch { }
+  }
 }
